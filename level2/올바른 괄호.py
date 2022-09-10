91e90f40daca89e15d1183fed076534911f9810d
YIHYUN HA <hihyun16@gmail.com>
2022-09-10 23:27:16 +0900

    Add solution of [올바른 괄호] - 스택/큐 - 순차 탐색

---
+++ level2/올바른 괄호.py
@@ -0,0 +1,15 @@
+def solution(s):
+    answer = True
+    left = 0
+    right = 0
+    for i in s:
+        if i == '(':
+            left += 1
+        elif i == ')':
+            right += 1
+        if right > left:
+            answer = False
+            break
+    if left != right:
+        answer = False
+    return answer
