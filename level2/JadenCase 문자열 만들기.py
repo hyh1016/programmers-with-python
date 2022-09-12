5ac5ebbbfea33933977681aa1ae9193582ae8a64
YIHYUN HA <hihyun16@gmail.com>
2022-09-12 14:10:17 +0900

    Add solution of [JadenCase 문자열 만들기] - 문자열 - 순차 탐색

---
+++ level2/JadenCase 문자열 만들기.py
@@ -0,0 +1,14 @@
+def solution(s):
+    words = s.split(' ')
+    new_words = []
+    for word in words:
+        if len(word) == 0:
+            new_words.append('')
+            continue
+        new_word = word[0].upper()
+        for i in range(1, len(word)):
+            new_word += word[i].lower()
+        new_words.append(new_word)
+    return ' '.join(new_words)
+
+print(solution("3people unFollowed me"))
